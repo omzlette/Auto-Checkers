1036f2ebfb98f81378cee24576695330ad56a2af
omzlette <tharnarch.omz@mail.kmutt.ac.th>
2024-05-07 16:54:33 +0900

    separate codes into modules

---
+++ checkers.py
@@ -0,0 +1,80 @@
+import pygame
+from pygame.locals import *
+import copy
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BROWN = (133, 84, 49)
+LIGHT_BROWN = (252, 230, 215)
+
+width, height = 400, 400
+rows, cols = 8, 8
+squareSize = width // rows
+
+class Checkers:
+    def __init__(self, turn='b', board=None):
+        pygame.init()
+
+        self.windowsName = 'Checkers'
+        self.screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption(self.windowsName)
+
+        self.movesDone = {'b': [], 'w': []}
+        if board is None:
+            self.board = [['-', 'b', '-', 'b', '-', 'b', '-', 'b'],
+                          ['b', '-', 'b', '-', 'b', '-', 'b', '-'],
+                          ['-', '-', '-', '-', '-', '-', '-', '-'],
+                          ['-', '-', '-', '-', '-', '-', '-', '-'],
+                          ['-', '-', '-', '-', '-', '-', '-', '-'],
+                          ['-', '-', '-', '-', '-', '-', '-', '-'],
+                          ['-', 'w', '-', 'w', '-', 'w', '-', 'w'],
+                          ['w', '-', 'w', '-', 'w', '-', 'w', '-']]
+            self.prevBoard = copy.deepcopy(self.board)
+            # self.board = [['-', '-', '-', '-', '-', '-', '-', '-'],
+            #               ['-', '-', '-', '-', '-', '-', '-', '-'],
+            #               ['-', '-', '-', '-', '-', '-', '-', '-'],
+            #               ['-', '-', '-', '-', '-', '-', '-', '-'],
+            #               ['-', '-', '-', '-', '-', '-', '-', '-'],
+            #               ['-', '-', '-', '-', '-', '-', '-', '-'],
+            #               ['-', '-', '-', '-', '-', 'B', '-', '-'],
+            #               ['-', '-', '-', '-', '-', '-', 'B', '-']]
+        else:
+            self.board = board
+        self.turn = turn # current turn
+    
+    ## Draw board and pieces
+    def draw_board(self):
+        for row in range(rows):
+            for col in range(row % 2, cols, 2):
+                pygame.draw.rect(self.screen, LIGHT_BROWN, (col * squareSize, row * squareSize, squareSize, squareSize))
+
+    def draw_pieces(self):
+        for row in range(rows):
+            for col in range(cols):
+                font = pygame.font.SysFont('Arial', 20)
+                text = font.render('K', True, (0, 120, 255))
+                if self.board[row][col] == 'b':
+                    pygame.draw.circle(self.screen, BLACK, (col * squareSize + squareSize // 2, row * squareSize + squareSize // 2), squareSize // 2 - 10)
+                elif self.board[row][col] == 'w':
+                    pygame.draw.circle(self.screen, WHITE, (col * squareSize + squareSize // 2, row * squareSize + squareSize // 2), squareSize // 2 - 10)
+                elif self.board[row][col] == 'B':
+                    pygame.draw.circle(self.screen, BLACK, (col * squareSize + squareSize // 2, row * squareSize + squareSize // 2), squareSize // 2 - 10)
+                    # pygame.draw.circle(self.screen, WHITE, (col * squareSize + squareSize // 2, row * squareSize + squareSize // 2), squareSize // 2 - 20)
+                    self.screen.blit(text, (col * squareSize + squareSize // 2 - text.get_width() // 2, row * squareSize + squareSize // 2 - text.get_height() // 2))
+                elif self.board[row][col] == 'W':
+                    pygame.draw.circle(self.screen, WHITE, (col * squareSize + squareSize // 2, row * squareSize + squareSize // 2), squareSize // 2 - 10)
+                    # pygame.draw.circle(self.screen, BLACK, (col * squareSize + squareSize // 2, row * squareSize + squareSize // 2), squareSize // 2 - 20)
+                    self.screen.blit(text, (col * squareSize + squareSize // 2 - text.get_width() // 2, row * squareSize + squareSize // 2 - text.get_height() // 2))
+    
+    def debug_text(self):
+        squareCount = 0
+        for row in range(rows):
+            for col in range((row % 2)-1, cols, 2):
+                if col != -1:
+                    squareCount += 1
+                    font = pygame.font.SysFont('Arial', 20)
+                    text = font.render(f'{squareCount}', True, (255, 0, 0))
+                    self.screen.blit(text, (col * squareSize + squareSize - text.get_width(), row * squareSize + squareSize - text.get_height()))
+
+    def updateMovesDict(self, piece, move, turn):
+        self.movesDone[turn].append([piece, move])
---
+++ main.py
@@ -0,0 +1,98 @@
+import pygame
+from pygame.locals import *
+from checkers import *
+from player import *
+
+def main():
+    INITIAL_DEPTH = 5
+    isGameOver = False
+    running = True
+    nummoves = 0
+
+    board = Checkers()
+
+    player1 = User('b', board.board, board.movesDone)
+    # player1 = randomBot('b', board.board, board.movesDone)
+    # player1 = Minimax('b', INITIAL_DEPTH, board.board, board.movesDone)
+    # player1 = AlphaBeta('b', INITIAL_DEPTH, board.board, board.movesDone)
+    
+    # player2 = User('w', board.board, board.movesDone)
+    # player2 = randomBot('w', board.board, board.movesDone)
+    # player2 = Minimax('w', INITIAL_DEPTH, board.board, board.movesDone)
+    player2 = AlphaBeta('w', INITIAL_DEPTH, board.board, board.movesDone)
+    
+    while running:
+        isGameOver = is_game_over(board.board, board.movesDone)
+
+        board.screen.fill(BROWN)
+        board.draw_board()
+        board.draw_pieces()
+        board.debug_text()
+        pygame.display.flip()
+
+        if not isGameOver:
+            if board.turn == 'b':
+                if player1.user:
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                            print('Early Exiting...')
+                            running = False
+                        if event.type == pygame.MOUSEBUTTONDOWN:
+                            row, col = player1.get_mouse_pos()
+                            board.board, board.turn, selectedPiece = player1.handle_mouse_click(row, col, board.board)
+                            player1.turn = board.turn
+                            player2.turn = board.turn
+                else:
+                    bestPiece, bestMove = player1.play(board.board)
+                    if bestPiece is not None and bestMove is not None:
+                        player1.prevCount = countBlack(board.board) + countWhite(board.board)
+                        board.board, board.turn = player1.update_board(board.board, bestPiece, bestMove)
+                        player1.turn = board.turn
+                        player2.turn = board.turn
+                
+            elif board.turn == 'w':
+                if player2.user:
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                            print('Early Exiting...')
+                            running = False
+                        if event.type == pygame.MOUSEBUTTONDOWN:
+                            row, col = player2.get_mouse_pos()
+                            board.board, board.turn, selectedPiece = player2.handle_mouse_click(row, col, board.board)
+                            player1.turn = board.turn
+                            player2.turn = board.turn
+                else:
+                    bestPiece, bestMove = player2.play(board.board)
+                    if bestPiece is not None and bestMove is not None:
+                        player2.prevCount = countBlack(board.board) + countWhite(board.board)
+                        board.board, board.turn = player2.update_board(board.board, bestPiece, bestMove)
+                        player1.turn = board.turn
+                        player2.turn = board.turn
+
+
+            if board.board != board.prevBoard:
+                nummoves += 1
+                board.prevBoard = copy.deepcopy(board.board)
+                if board.turn == 'w':
+                    board.updateMovesDict(tuple(selectedPiece), (row, col), 'b')
+                else:
+                    board.updateMovesDict(tuple(bestPiece), tuple(bestMove), 'w')
+                print('Moves Done:', board.movesDone)
+                print('Moves:', nummoves, 'Turn:', board.turn)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                running = False
+                if countBlack(board.board) > countWhite(board.board) or isGameOver == 'b':
+                    print('Game Over, Winner: Black')
+                    # winnerData(loop, 'b', filename)
+                elif countBlack(board.board) < countWhite(board.board) or isGameOver == 'w':
+                    print('Game Over, Winner: White')
+                    # winnerData(loop, 'w', filename)
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+                print('Restarting...')
+                main()
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()

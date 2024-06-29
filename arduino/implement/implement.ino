38d946c05c6edaf4c226a6c42c94f74e589188ab
omzlette <tharnarch.omz@mail.kmutt.ac.th>
2024-06-29 15:05:22 +0900

    Added millis() + timer interrupts

--- arduino/implement/implement.ino
+++ arduino/implement/implement.ino
@@ -23,23 +23,23 @@
 #define HALF_OF_SQUARES 16
 #define SQUARES 32
 
-#define LED1 22 // On/Off
-#define LED2 23 // Start
-#define LED3 24 // Set Home
-#define LED4 25 // Ready
-#define LED5 26 // Running
-#define LED6 27 // Error
-#define LED7 28 // Black Turn, win = blink
-#define LED8 29 // White Turn, win = blink
+#define LED1_ONOFF 22 // On/Off
+#define LED2_START 23 // Start
+#define LED3_SETHOME 24 // Set Home
+#define LED4_READY 25 // Ready
+#define LED5_RUNNING 26 // Running
+#define LED6_ERROR 27 // Error
+#define LED7_BTURN 28 // Black Turn, win = blink
+#define LED8_WTURN 29 // White Turn, win = blink
 #define LED9 30
-#define LED10 31 // Draw
+#define LED10_DRAW 31 // Draw
 
 #define LIMIT_SWITCH1 34
 #define LIMIT_SWITCH2 36
-#define START_BUTTON 35
-#define STOP_BUTTON 37
-#define RESET_BUTTON 39
-#define SETHOME_BUTTON 41
+#define START_BUTTON 18
+#define STOP_BUTTON 19
+#define RESET_BUTTON 20
+#define SETHOME_BUTTON 21
 
 #define MAX_BUFFER_LENGTH 32
 
@@ -59,47 +59,55 @@ Max Linear Velocity : 0.1 sqrt2 m/s
 Max Angular Velocity : 19.7378 rad/s
 */
 
+static unsigned long _millis = 0;
+
 const float MICROSTEPS = 8;
 const float maxSPS = 5026.19; // max angular vel * steps per rev / 2PI
 
 const float movementGap = 888.51; // half a square == 25 mm -> (microstepsPerRev * half of square / pulleyRad * 2PI) usteps
 
-float desiredPos[2] = {0, 0};
+long desiredPos[2] = {0, 0};
 bool toDesignatedPos = false;
+bool reachedDesignatedPos = false;
+
+bool drawStatus = false;
+bool whiteWinStatus = false;
+bool blackWinStatus = false;
+bool blackTurnStatus = false;
+bool whiteTurnStatus = false;
+bool errorStatus = false;
+bool illegalMoveStatus = false;
+
+bool startButton = false;
+bool stopButton = false;
+bool resetButton = false;
+bool setHomeButton = false;
+bool limitSwitch1 = false;
+bool limitSwitch2 = false;
 
 enum {CONNECTED, DISCONNECTED} connection = DISCONNECTED;
-enum {INIT, IDLE, RUNNING, ERROR} state = INIT;
+enum {SETHOME, START_IDLE, RESET_IDLE, IDLE, MOTOR_RUNNING, ERROR} state = SETHOME;
 
 const char DELIMITER = ';';
 const uint8_t ACK = 0x50;
 const uint8_t CMPLT = 0x51;
 const uint8_t NACK = 0x60;
 
-// Declare Functions
+// Functions
+unsigned long Millis();
 
 void initialize();
 void setupStepper();
 
 void sendBoardData();
+void motorMovement(const char * data, const unsigned int dataLength);
+void updateStatus(const char status);
 
-void stateManagement(const byte mode, const char * data);
+void stateManagement(const byte mode, const char * data, const unsigned int dataLength);
 void processIncomingByte(const byte inByte);
 
 void setup() {
   cli();
-
-  // Set timer0 interrupt at 1MHz
-  TCCR0A = 0; // set entire TCCR0A register to 0
-  TCCR0B = 0; // same for TCCR0B
-  TCNT0  = 0; // initialize counter value to 0
-  // set compare match register for 1Mhz increments
-  OCR0A = 15; // = (16*10^6) / (1*10^6) - 1 (must be <256)
-  // turn on CTC mode
-  TCCR0A |= (1 << WGM01);
-  // Set CS01 and CS00 bits for 1 prescaler
-  TCCR0B |= (1 << CS00);
-  // enable timer compare interrupt
-  TIMSK0 |= (1 << OCIE0A);
 
   // Set timer1 interrupt at 10kHz
   TCCR1A = 0; // set entire TCCR1A register to 0
@@ -110,35 +118,65 @@ void setup() {
   // turn on CTC mode
   TCCR1B |= (1 << WGM12);
   // Set CS12, CS11 and CS10 bits for 1 prescaler
-  TCCR1B |= (1 << CS10)
+  TCCR1B |= (1 << CS10);
   // enable timer compare interrupt
   TIMSK1 |= (1 << OCIE1A);
+
+  // Set timer2 interrupt at 10kHz
+  TCCR2A = 0; // set entire TCCR0A register to 0
+  TCCR2B = 0; // same for TCCR0B
+  TCNT2  = 0; // initialize counter value to 0
+  // set compare match register for 10khz increments
+  OCR2A = 124; // = (16*10^6) / prescaler / (1*10^3) - 1 (must be <256)
+  // turn on CTC mode
+  TCCR2A |= (1 << WGM21);
+  // Set 128 prescaler
+  TCCR2B |= (1 << CS22) | (1 << CS20);
+  // enable timer compare interrupt
+  TIMSK2 |= (1 << OCIE2A);
 
   // Initialize everything
   initialize();
   setupStepper();
 
-  // Start interrupt
+  // Set up serial communication
+  // Serial.begin(115200);
+
+  // Start interrupts
   sei();
 
 }
 
-ISR(TIMER0_COMPA_vect) {
-  if(toDesignatedPos){
+ISR(TIMER1_COMPA_vect) {
+  if(toDesignatedPos && !reachedDesignatedPos){
     steppers.moveTo(desiredPos);
+    toDesignatedPos = false;
   }
+
+  if(!steppers.run()){
+    // reached the designated position
+    reachedDesignatedPos = true;
+  }
+
   steppers.run();
 }
 
-ISR(TIMER1_COMPA_vect) {
-  // Get the current board state from the mux
-  // might not be needed
+ISR(TIMER2_COMPA_vect) {
+  _millis++;
+  if (_millis % 1000 == 0){
+    digitalWrite(LED9, !digitalRead(LED9));
+    digitalWrite(LED10_DRAW, !digitalRead(LED10_DRAW));
+  }
 }
 
 void loop() {
-  while(Serial.available() > 0){
-    processIncomingByte(Serial.read());
-  }
+  // while(Serial.available() > 0){
+  //   processIncomingByte(Serial.read());
+  // }
+}
+
+unsigned long Millis(){
+  return _millis;
 }
 
 void initialize(){
@@ -170,16 +208,16 @@ void initialize(){
   /*
   Set up the LEDs
   */
-  pinMode(LED1, OUTPUT); digitalWrite(LED1, LOW);
-  pinMode(LED2, OUTPUT); digitalWrite(LED2, LOW);
-  pinMode(LED3, OUTPUT); digitalWrite(LED3, LOW);
-  pinMode(LED4, OUTPUT); digitalWrite(LED4, LOW);
-  pinMode(LED5, OUTPUT); digitalWrite(LED5, LOW);
-  pinMode(LED6, OUTPUT); digitalWrite(LED6, LOW);
-  pinMode(LED7, OUTPUT); digitalWrite(LED7, LOW);
-  pinMode(LED8, OUTPUT); digitalWrite(LED8, LOW);
+  pinMode(LED1_ONOFF, OUTPUT); digitalWrite(LED1_ONOFF, LOW);
+  pinMode(LED2_START, OUTPUT); digitalWrite(LED2_START, LOW);
+  pinMode(LED3_SETHOME, OUTPUT); digitalWrite(LED3_SETHOME, LOW);
+  pinMode(LED4_READY, OUTPUT); digitalWrite(LED4_READY, LOW);
+  pinMode(LED5_RUNNING, OUTPUT); digitalWrite(LED5_RUNNING, LOW);
+  pinMode(LED6_ERROR, OUTPUT); digitalWrite(LED6_ERROR, LOW);
+  pinMode(LED7_BTURN, OUTPUT); digitalWrite(LED7_BTURN, LOW);
+  pinMode(LED8_WTURN, OUTPUT); digitalWrite(LED8_WTURN, LOW);
   pinMode(LED9, OUTPUT); digitalWrite(LED9, LOW);
-  pinMode(LED10, OUTPUT); digitalWrite(LED10, LOW);
+  pinMode(LED10_DRAW, OUTPUT); digitalWrite(LED10_DRAW, LOW);
 }
 
 void setupStepper(){
@@ -216,7 +254,7 @@ void sendBoardData(){
     int mux1Val = analogRead(MUX1_SIG);
     int mux2Val = analogRead(MUX2_SIG);
     if (mux1Val > MUX_THRESHOLD){
-      switch(floor(idx / 4) % 2){
+      switch((int) floor(idx / 4) % 2){
         case 0:
           boardState_row1 |= ((idx % 4) * 2) + 1;
           break;
@@ -226,7 +264,7 @@ void sendBoardData(){
       }
     }
     if (mux2Val > MUX_THRESHOLD){
-      switch(floor(idx / 4) % 2){
+      switch((int) floor(idx / 4) % 2){
         case 0:
           boardState_row2 |= ((idx % 4) * 2) + 1;
           break;
@@ -237,8 +275,8 @@ void sendBoardData(){
     }
 
     if (idx % 4 == 3){
-      boardState_buffer[floor(idx / 4)] = boardState_row1;
-      boardState_buffer[floor(idx / 4) + 4] = boardState_row2;
+      boardState_buffer[(int) floor(idx / 4)] = boardState_row1;
+      boardState_buffer[(int) floor(idx / 4) + 4] = boardState_row2;
       boardState_row1 = 0;
       boardState_row2 = 0;
     }
@@ -248,14 +286,96 @@ void sendBoardData(){
   Serial.write(DELIMITER);
 }
 
-void stateManagement(const byte mode, const char * data){
+void motorMovement(const char * data, const unsigned int dataLength){
+  /*
+  Move the stepper motors to the designated position
+  */
+  // WIP
+  // desiredPos[0] = x * movementGap;
+  // desiredPos[1] = y * movementGap;
+
+  // toDesignatedPos = true;
+  // reachedDesignatedPos = false;
+}
+
+void updateStatus(const char status){
+  /*
+  Update board's status
+  From LSB to MSB
+  0: draw
+  1: white win
+  2: black win
+  4: black turn
+  5: white turn
+  6: error
+  7: illegal move (blink, error led)
+  */
+  for(int i = 0; i < 8; i++){
+    if (status & (1 << i)){
+      switch(i){
+        case 0:
+          drawStatus = true;
+          break;
+        case 1:
+          whiteWinStatus = true;
+          break;
+        case 2:
+          blackWinStatus = true;
+          break;
+        case 4:
+          blackTurnStatus = true;
+          break;
+        case 5:
+          whiteTurnStatus = true;
+          break;
+        case 6:
+          errorStatus = true;
+          break;
+        case 7:
+          // illegal move (blink)
+          illegalMoveStatus = true;
+          break;
+      }
+    }
+    else{
+      switch(i){
+        case 0:
+          drawStatus = false;
+          break;
+        case 1:
+          whiteWinStatus = false;
+          break;
+        case 2:
+          blackWinStatus = false;
+          break;
+        case 4:
+          blackTurnStatus = false;
+          break;
+        case 5:
+          whiteTurnStatus = false;
+          break;
+        case 6:
+          errorStatus = false;
+          break;
+        case 7:
+          // illegal move (blink)
+          illegalMoveStatus = false;
+          break;
+      }
+    }
+  }
+}
+
+void stateManagement(const byte mode, const char * data, const unsigned int dataLength){
   /*
   Process the data received from the Jetson
   */
   switch (connection){
     case DISCONNECTED:
+      digitalWrite(LED1_ONOFF, LOW);
       switch (mode){
         case 0xF0:  // Connection request
+          digitalWrite(LED1_ONOFF, HIGH);
           connection = CONNECTED;
           Serial.write(ACK);
           break;
@@ -266,26 +386,69 @@ void stateManagement(const byte mode, const char * data){
       break;
     
     case CONNECTED:
-      switch (mode){
-        // Mode 0xF1: Requested for board state
-        case 0xF1:
-          sendBoardData();
-          // then wait for ack
+      switch(state){
+        case SETHOME:
+          digitalWrite(LED4_READY, LOW);
+          // button press
+
+          // sethome
+
+          // to START_IDLE
+          state = START_IDLE;
           break;
-        // Mode 0xF2: Requested for stepper movement
-        case 0xF2:
+        case START_IDLE:
+          digitalWrite(LED4_READY, LOW);
+          // button press
+
+          // start
+          Serial.write(0x99);
+
+          // to IDLE
+          state = IDLE;
           break;
-        // Mode 0xF3: Requested for status indication
-        case 0xF3:
+        case RESET_IDLE:
+          digitalWrite(LED4_READY, LOW);
+          // button press
+
+          // reset
+
+          // to IDLE
+          state = SETHOME;
           break;
-        // Mode 0xFF: Disconnection request
-        case 0xFF:
-          connection = DISCONNECTED;
-          Serial.write(ACK);
+        case IDLE:
+          digitalWrite(LED4_READY, HIGH);
+          switch (mode){
+            // Mode 0xF1: Requested for board state
+            case 0xF1:
+              sendBoardData();
+              // then wait for ack
+              break;
+            // Mode 0xF2: Requested for stepper movement
+            case 0xF2:
+              state = MOTOR_RUNNING;
+              break;
+            // Mode 0xF3: Requested for status indication
+            case 0xF3:
+              updateStatus(data[0]);
+              break;
+            // Mode 0xFF: Disconnection request
+            case 0xFF:
+              connection = DISCONNECTED;
+              Serial.write(ACK);
+              break;
+            // invalid mode (unknown command, should not happen)
+            default:
+              Serial.write(NACK);
+              break;
+          }
           break;
-        // invalid mode (unknown command, should not happen)
-        default:
-          Serial.write(NACK);
+        case MOTOR_RUNNING:
+          digitalWrite(LED4_READY, LOW);
+          digitalWrite(LED5_RUNNING, HIGH);
+          // set motor track (1 byte per motor)
+
+          // if completed
+          state = IDLE;
           break;
       }
       break;
@@ -319,7 +482,7 @@ void processIncomingByte(const byte inByte){
       if (calculatedChecksum == checksum){
         // Checksum is correct, send ACK and process the data
         Serial.write(ACK);
-        stateManagement(mode, dataBuffer);
+        stateManagement(mode, dataBuffer, bufferIDX - 1);
       }
       else{
         // Checksum is incorrect, send NACK

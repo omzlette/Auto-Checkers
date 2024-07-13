688520eb3d084dec58af6c00be52e054df2a8e3a
omzlette <tharnarch.omz@mail.kmutt.ac.th>
2024-07-13 23:09:41 +0900

    Added motor experiment test code

--- arduino/encoder-experiment/encoder-experiment.ino
+++ arduino/encoder-experiment/encoder-experiment.ino
@@ -1,28 +1,14 @@
 #include <Wire.h>
 #include <AS5600.h>
-#include <eRCaGuy_Timer2_Counter.h>
 
 #define DIR_PIN 2
-#define BUTTON 4
+#define BUTTON 13
 
 AMS_5600 encoder;
 
 bool startFlag = false;
 
-float currentAngle = 0;
-float previousAngle = 0;
-float continuousAngle = 0.0;
-float prevContAngle = 0.0;
-int numRevolutions = 0;
-float timeLast = 0;
-float timeNow = 0;
-float pulleyRadius = 0.007; // meters
-
-unsigned long now = 0;
-unsigned long freq = 1000; // kHz (1000 = mega)
-unsigned long timeDelay = 1000 / freq; // us
-
-int debugIDX = 0;
+volatile unsigned long now = 0;
 
 float mapfloat(long x, long in_min, long in_max, long out_min, long out_max)
 {
@@ -36,46 +22,40 @@ float Angle(){
 }
 
 void output(long rawAngle){
-  String buffer = String(rawAngle) + "," + String(now) + "," + String(digitalRead(8));
+  String buffer = String(rawAngle) + "," + String(now) + "," + String(digitalRead(BUTTON));
   Serial.println(buffer);
 }
 
 void setup() {
-  timer2.setup();
+  cli();
+  //set timer0 interrupt at 1kHz
+  TCCR0A = 0;// set entire TCCR0A register to 0
+  TCCR0B = 0;// same for TCCR0B
+  TCNT0  = 0;//initialize counter value to 0
+  // set compare match register for 1khz increments
+  OCR0A = 249;// = (16*10^6) / (1000*64) - 1 (must be <256)
+  // turn on CTC mode
+  TCCR0A |= (1 << WGM01);
+  // Set CS01 and CS00 bits for 64 prescaler
+  TCCR0B |= (1 << CS01) | (1 << CS00);   
+  // enable timer compare interrupt
+  TIMSK0 |= (1 << OCIE0A);
 
   pinMode(DIR_PIN, OUTPUT);
-  pinMode(BUTTON, INPUT_PULLUP);
-  pinMode(8, INPUT);
-  pinMode(9, INPUT);
-  Serial.begin(1000000);
+  pinMode(BUTTON, INPUT);
+  Serial.begin(115200);
   Wire.begin();
   Wire.setClock(400000);
 
-  Serial.println("---------");
-  Serial.println("Raw Input, Time, Started");
-  digitalWrite(DIR_PIN, HIGH);
+  sei();
+}
+
+ISR(TIMER0_COMPA_vect){
+  now++;
+  encoder.update();
+  output(encoder.getRawAngle());
 }
 
 void loop() {
-  int buttonState = !digitalRead(BUTTON);
-  if(buttonState == 1 && startFlag == false){
-    startFlag = true;
-    timeLast = timer2.get_count()/2;
-  }
 
-  if(startFlag){
-    if((timer2.get_count() - now)/2 >= timeDelay){
-      now = timer2.get_count();
-      // currentAngle = Angle();
-      // if(currentAngle - previousAngle > 180){
-      //   numRevolutions--;
-      // } else if(currentAngle - previousAngle < -180){
-      //   numRevolutions++;
-      // }
-      // continuousAngle = currentAngle + (numRevolutions * 360);
-
-      // previousAngle = currentAngle;
-      output(encoder.getRawAngle());
-    }
-  }
 }
--- arduino/newTest/newTest.ino
+++ arduino/newTest/newTest.ino
@@ -1,54 +1,180 @@
-void setup(){
-  pinMode(31, OUTPUT);
-  pinMode(30, OUTPUT);
-  pinMode(18, INPUT_PULLUP);
-  pinMode(19, INPUT_PULLUP);
+#include <AccelStepper.h>
+#include <MultiStepper.h>
 
-  attachInterrupt(digitalPinToInterrupt(18), []{if(digitalRead(18) == 0){digitalWrite(31, HIGH);}}, FALLING);
-  attachInterrupt(digitalPinToInterrupt(19), OFF, FALLING);
+#define DRV1_EN 52
+#define DRV2_EN 53
+
+#define DRV1_DIR 2
+#define DRV1_STEP 3
+#define DRV2_DIR 4
+#define DRV2_STEP 5
+
+volatile unsigned long _millis = 0;
+volatile unsigned long moveUpLast = 0;
+volatile unsigned long startCntDown = 0;
+
+const float MICROSTEPS = 8;
+const float maxSPS = 5026.19; // max angular vel * steps per rev / 2PI
+
+const float movementGap = 888.51; // half a square == 25 mm -> (microstepsPerRev * half of square / pulleyRad * 2PI) usteps
+
+volatile unsigned long last = 0;
+volatile unsigned long deltaDist = 0;
+volatile long currPos = 0;
+volatile float vel = 0;
+volatile bool Started = false;
+volatile bool moveUp = false;
+volatile bool changeDir = false;
+volatile bool buttonPressed = false;
+
+AccelStepper stepperX(1, DRV1_STEP, DRV1_DIR);
+AccelStepper stepperY(1, DRV2_STEP, DRV2_DIR);
+MultiStepper steppers;
+
+unsigned long Millis();
+
+void setup(){
+  cli();
+  // Set timer1 interrupt at 10kHz
+  TCCR1A = 0; // set entire TCCR1A register to 0
+  TCCR1B = 0; // same for TCCR1B
+  TCNT1  = 0; // initialize counter value to 0
+  // set compare match register for 10khz increments
+  OCR1A = 1599; // = (16*10^6) / (1*10^4) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS12, CS11 and CS10 bits for 1 prescaler
+  TCCR1B |= (1 << CS10);
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+
+  // Set timer2 interrupt at 1kHz
+  TCCR2A = 0; // set entire TCCR0A register to 0
+  TCCR2B = 0; // same for TCCR0B
+  TCNT2  = 0; // initialize counter value to 0
+  // set compare match register for 1khz increments
+  OCR2A = 124; // = (16*10^6) / prescaler / (1*10^3) - 1 (must be <256)
+  // turn on CTC mode
+  TCCR2A |= (1 << WGM21);
+  // Set 128 prescaler
+  TCCR2B |= (1 << CS22) | (1 << CS20);
+  // enable timer compare interrupt
+  TIMSK2 |= (1 << OCIE2A) | (1 << OCIE2B);
+
+  pinMode(18, INPUT_PULLUP);
+  pinMode(19, OUTPUT); digitalWrite(19, LOW);
+  pinMode(DRV1_EN, OUTPUT); digitalWrite(DRV1_EN, LOW);
+
+  attachInterrupt(digitalPinToInterrupt(18), Start, FALLING);
+
+  stepperX.setMaxSpeed(maxSPS);
+  stepperX.setSpeed(maxSPS);
+  stepperX.setAcceleration(maxSPS);
 
   Serial.begin(115200);
+
+  sei();
 }
 
-void loop(){
-  digitalWrite(30, HIGH);
-  delay(100);
-  digitalWrite(30, LOW);
-  delay(100);
+ISR(TIMER1_COMPA_vect){
+  // if(buttonPressed){
+  //   if(!moveUp && !Started){
+  //     stepperX.runSpeed();
+  //     moveUp = true;
+  //     moveUpLast = Millis();
+  //   }
+  //   else if(moveUp && !Started){
+  //     stepperX.runSpeed();
+  //     if(Millis() - moveUpLast >= 10){
+  //       stepperX.stop();
+  //       Started = true;
+  //       startCntDown = Millis();
+  //     }
+  //   }
+
+  //   if(Started){
+  //     if(Millis() - startCntDown >= 1000){
+  //       if(!changeDir){
+  //         stepperX.moveTo(movementGap * 15);
+  //         changeDir = true;
+  //       }
+  //       else if(changeDir && stepperX.distanceToGo() == 0){
+  //         stepperX.moveTo(0);
+  //       }
+  //       stepperX.run();
+  //     }
+  //   }
+  
+  // }
 }
 
-void ON(){
-  digitalWrite(31, HIGH);
+ISR(TIMER2_COMPA_vect) {
+  _millis++;
 }
 
-void OFF(){
-  if(digitalRead(19) == 0){
-    Serial.println("OFF Pressed");
-    digitalWrite(31, LOW);
+ISR(TIMER2_COMPB_vect) {
+  // debug
+  if(buttonPressed){
+    static volatile unsigned long now = Millis();
+
+    deltaDist = stepperX.distanceToGo();
+    currPos = stepperX.currentPosition();
+    vel = stepperX.speed();
+
+        Serial.print(currPos);
+        Serial.print(",");
+        Serial.print(vel);
+        Serial.print(",");
+        Serial.print(now);
+
+        Serial.print(",");
+        Serial.println(Started);
   }
 }
 
-// void debug(){
-//   if(DEBUG){
-//     if(micros() - now >= timeDelay){
-//       now = micros();
+unsigned long Millis(){
+  return _millis;
+}
 
-//       float deltaT = (now - last);
+void loop(){
+  
+  // if(buttonPressed){
+  //   if(!moveUp && !Started){
+  //     stepperX.runSpeed();
+  //     moveUp = true;
+  //     moveUpLast = Millis();
+  //   }
+  //   else if(moveUp && !Started){
+  //     stepperX.runSpeed();
+  //     if(Millis() - moveUpLast >= 10){
+  //       stepperX.stop();
+  //       Started = true;
+  //       startCntDown = Millis();
+  //     }
+  //   }
 
-//       deltaDist_S1 = stepper1.distanceToGo();
-//       currPos_S1 = stepper1.currentPosition();
-//       vel_S1 = stepper1.speed();
+  //   if(Started){
+  //     if(Millis() - startCntDown >= 1000){
+  //       if(!changeDir){
+  //         stepperX.moveTo(movementGap * 15);
+  //         changeDir = true;
+  //       }
+  //       else if(changeDir && stepperX.distanceToGo() == 0){
+  //         stepperX.moveTo(0);
+  //       }
+  //       stepperX.run();
+  //     }
+  //   }
+  
+  // }
+  // stepperX.runSpeed();
+}
 
-//           Serial.print(currPos_S1);
-//           Serial.print(",");
-//           Serial.print(vel_S1);
-//           Serial.print(",");
-//           Serial.print(now);
+void Start(){
+  if(digitalRead(18) == 0){
+    buttonPressed = true;
+    digitalWrite(19, HIGH);
 
-//           Serial.print(",");
-//           Serial.println(Started);
-
-//       last = micros();
-//     }
-//   }
-// }
+    digitalWrite(DRV1_EN, HIGH);
+  }
+}
